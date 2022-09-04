b0db3a785e911f960a24a9e6df471f445fde0075
Felipedev <felipesalazar.dev@gmail.com>
2022-09-04 12:00:10 -0500

    version completa

--- myproject/main.js
+++ myproject/main.js
@@ -3,7 +3,9 @@ import "./style.css";
 import * as THREE from "three";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { BoxGeometry, MathUtils, MeshStandardMaterial, SphereGeometry } from "three";
+
+// Setup
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -20,14 +22,19 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 renderer.render(scene, camera);
 
+// Torus
+
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff637 });
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+// Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
@@ -35,13 +42,15 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// Helpers
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
-function addStart() {
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
@@ -53,10 +62,63 @@ function addStart() {
   scene.add(star);
 }
 
-Array(200).fill().forEach(addStart);
+Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load("space-bg.jpg");
 scene.background = spaceTexture;
+
+//  Profile
+
+const philipTexture = new THREE.TextureLoader().load("profilevector.webp");
+
+const philip = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: philipTexture })
+);
+scene.add(philip);
+
+// Moon
+const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+philip.position.z = -5;
+philip.position.x = 2;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  philip.rotation.y += 0.01;
+  philip.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
 
 function animate() {
   requestAnimationFrame(animate);
@@ -65,33 +127,11 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  controls.update();
+  moon.rotation.x += 0.005;
+
+  // controls.update();
 
   renderer.render(scene, camera);
 }
 
 animate();
-
-//  Profile
-
-const philipTexture = new THREE.TextureLoader().load("jeff.png");
-
-const philip = new THREE.Mesh(
-  new THREE.BoxGeometry(3, 3, 3),
-  new THREE.MeshBasicMaterial({ map: philipTexture })
-);
-
-// Moon
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3,32,32),
-  new THREE.MeshStandardMaterial( {
-    map: moonTexture,
-    normalMap: normalTexture
-  })
-)
-
-// scene.add(philip);
-scene.add(moon);
